36eb12137d2664455f47bf43ca8fabcb65fe64f8
fixnetcodes <deogratiasamas@gmail.com>
2023-07-28 17:25:59 +0300

    create user model

--- model/User.php
+++ model/User.php
@@ -71,4 +71,93 @@ class User
         }
     }
     
+    //register user
+    public function register()
+    {
+        global $data;
+        
+        $userData = array(
+            'Name'      => str_replace("'","\'",$_POST['fullname']),
+            'Password'  =>md5( $_POST['password']),
+            'EmailId'   => str_replace("'","\'",$_POST['email']),
+            'UserType'  => str_replace("'","\'",$_POST['user_type']),
+            'MobileNo'  => $_POST['mobile'],
+        );
+        // echo json_encode($userData);
+        // exit;
+        
+        //count array data
+        $total_data = count($userData);
+        $update_query = '';
+        
+        if(!empty($_POST['id'])) 
+        {
+            foreach($userData as $columns => $values)
+            {
+                if($total_data > 1)
+                {
+                    $update_query .= "$columns";
+                    $update_query .= "=";
+                    $update_query .= "'";
+                    $update_query .= $values;
+                    $update_query .= "',";
+                    $total_data--;
+                }else
+                {
+                    $update_query .= "$columns";
+                    $update_query .= "=";
+                    $update_query .= "'";
+                    $update_query .= $values;
+                    $update_query .= "',";
+                }
+            }
+            if(!empty($_POST['password'])){	
+                $query="Update usermaster set $update_query where Id= ".$_POST['id']."";
+            }else{
+             $query="Update usermaster set 
+                 Name      = '".str_replace("'","\'",$_POST['fullname'])."',
+                EmailId   = '".str_replace("'","\'",$_POST['email'])."',
+                UserType  = '".str_replace("'","\'",$_POST['user_type'])."',
+                MobileNo  = '".$_POST['mobile']."'
+             where Id=".$_POST['id']."
+             ";
+            }
+            $stmt = $data->prepare($query);
+            $_SESSION['message']= '<div class="alert alert-success">User updated successfully...</div>'; 
+            ?>
+            <script>
+            window.history.back();
+            </script>
+            <?php
+        }else
+        {
+            foreach($userData as $key=>$value)
+		    {
+                if(is_null($value) || $value == '')
+                unset($userData[$key]);
+		    }
+            $fields  = implode(", ",array_keys($userData)); //fields for insert query
+            $values   = implode("','", array_values($userData)); //values for insert query
+            $query="INSERT INTO usermaster($fields) values('$values')";
+            $results = $data->prepare($query);
+            if($results) {
+                $_SESSION['message']='<div class="alert alert-success">User created successfully...</div>'; 
+                ?>
+                <script>
+                window.history.back();
+                </script>
+                <?php
+            } else {
+                $_SESSION['message']='<div class="alert alert-danger">Problem in user creation...</div>'; 
+                ?>
+                <script>
+                window.history.back();
+                </script>
+                <?php
+            }
+        }
+        
+        
+    }
+    
 }
